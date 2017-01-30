abccb33cfdf8801908f37636ae48724ad235c4af
curty <louis.curty26@gmail.com>
2017-01-30 17:40:10 +0100

    API list

--- app/src/main/java/com/example/louis/androidproject/MainActivity.java
+++ app/src/main/java/com/example/louis/androidproject/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.louis.androidproject;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -11,17 +13,38 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.louis.androidproject.model.GlobalObject;
+import com.example.louis.androidproject.tools.MyAdapter;
 import com.google.gson.Gson;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private String[] myDataset = {"@3067", "@3071"};
+    private ArrayList<GlobalObject> initialCities = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //new AqicnAsyncTask(MainActivity.this).execute("https://api.waqi.info/api/feed/@3067/obs.fr.json");
-        getDataFromUrl("https://api.waqi.info/api/feed/@3067/obs.fr.json");
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new MyAdapter(initialCities);
+        mRecyclerView.setAdapter(mAdapter);
+
+
+        for(int i=0; i<myDataset.length;i++) {
+            getDataFromUrl("https://api.waqi.info/api/feed/"+myDataset[i]+"/obs.fr.json");
+        }
     }
 
     private void getDataFromUrl(String url) {
@@ -37,7 +60,8 @@ public class MainActivity extends AppCompatActivity {
                     public void onResponse(String response) {
                         Gson gson = new Gson();
                         GlobalObject obj = gson.fromJson(response, GlobalObject.class);
-                        mTextView.setText(obj.getRxs().getObs().get(0).getMsg().getCity().getName());
+                        initialCities.add(obj);
+                        mAdapter.notifyDataSetChanged();
                     }
                 }, new Response.ErrorListener() {
             @Override
